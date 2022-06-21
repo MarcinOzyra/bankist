c9ad95a66071e370f057a11dcf6a4027428e0fa3
Marcin <ozi@post.pl>
2022-06-21 18:47:16 +0200

    tabbed component

--- script.js
+++ script.js
@@ -59,3 +59,17 @@ document.querySelector('.nav__links').addEventListener('click', e => {
   // console.log(e.target);
   // console.log(id);
 });
+
+//tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tabContent => tabContent.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+});
